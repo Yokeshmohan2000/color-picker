05e5479818b3f130309e2ccaebfb2d71d3acc4e7
Yokeshmohan2000 <121154167+Yokeshmohan2000@users.noreply.github.com>
2023-10-29 19:52:36 +0530

    first commit

---
+++ colorpicker.js
@@ -0,0 +1,17 @@
+function gray(){
+    document.getElementById("change-color").style="background-color:#e0e0e0";
+    document.getElementById('color-code').innerHTML="#e0e0e0"
+}
+function green(){
+    document.getElementById("change-color").style="background-color:#6fcf97";
+    document.getElementById('color-code').innerHTML="#6fcf97"
+}
+function blue(){
+    document.getElementById("change-color").style="background-color:#56ccf2";
+    document.getElementById('color-code').innerHTML="#56ccf2"
+}
+function pink(){
+    document.getElementById("change-color").style="background-color:#bb6bd9";
+    document.getElementById('color-code').innerHTML="#bb6bd9"
+ 
+}
